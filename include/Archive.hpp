06416a7161f52627c8fa21bc0830aa840809e5b9
DangeL <92173667+DangeL187@users.noreply.github.com>
2024-06-11 17:01:26 +0300

    Add files via upload

--- include/Archive.hpp
+++ include/Archive.hpp
@@ -1,10 +1,11 @@
 #ifndef CLASSSERIALIZATION_ARCHIVE_HPP
 #define CLASSSERIALIZATION_ARCHIVE_HPP
 
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <limits>
+#include <string>
+#include <vector>
 
 class Archive {
 public:
@@ -12,8 +13,27 @@ public:
     explicit Archive(std::istream& is): _os(nullptr), _is(&is) {}
 
     template<typename T>
+    void operator&(std::vector<T>& other) {
+        if (_os) {
+            size_t size = other.size();
+            *_os << size << '\n';
+            for (auto& element: other) {
+                (*this) & element;
+            }
+        } else if (_is) {
+            size_t size;
+            *_is >> size;
+            _is->ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            other.resize(size);
+            for (auto& element: other) {
+                (*this) & element;
+            }
+        }
+    }
+
+    template<typename T>
     void operator&(T& other) {
-        if constexpr (std::is_arithmetic_v<T> || std::is_same_v<T, std::string>) {
+        if constexpr (std::is_arithmetic_v<T> || std::is_same_v<T, std::string> || std::is_same_v<T, char>) {
             serialize_basic(other);
         } else {
             other.serialize(*this, 1);
--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,44 @@
 struct POS {
     DEFINE_SERIALIZE(x, y)
 
-    int x = 69;
-    int y = 187;
+    int x = 0;
+    int y = 0;
     POS() = default;
+    POS(int x, int y): x(x), y(y) {}
 };
 
 class MyClass {
 public:
-    DEFINE_SERIALIZE(_id, _name, _none)
+    DEFINE_SERIALIZE(_id, _name, _none, _boo, _ch, _test)
 
     MyClass() = default;
 
-    MyClass(int id, const std::string& name) {
+    MyClass(float id, const std::string& name) {
         _id = id;
         _name = name;
+        _none = POS(69, 187);
+        _boo = true;
+        _ch = 89;
+        _test = {1, 2, 3, 4, 5, 6};
     }
 
     void print() {
-        std::cout << "ID: [" << _id << "], Name: [" << _name << "], None: [" << _none.x << " " << _none.y << "]" << std::endl;
+        std::cout << "ID: [" << _id << "], Name: [" << _name << "], None: [" << _none.x << " " << _none.y << "]\n";
+        std::cout << _boo << " [" << _ch << "]\n";
+        for (auto& i: _test) std::cout << i << "#";
     }
 
 private:
-    int _id = 0;
+    double _id = 0;
+    std::vector<int> _test;
     std::string _name;
     POS _none;
+    bool _boo = false;
+    char _ch = 0;
 };
 
 int main() {
-    MyClass original(13, "Boost Serialization");
+    MyClass original(13.123, "Boost Serialization");
     serialization::serialize(original, "data.txt");
 
     MyClass loaded;
