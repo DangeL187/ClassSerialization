ce7883071d48ad7b4d99c9c2fb1fe7e432249c14
DangeL <92173667+DangeL187@users.noreply.github.com>
2024-06-17 21:28:13 +0300

    Add files via upload

--- src/Archive/Archive.cpp
+++ src/Archive/Archive.cpp
@@ -1,15 +1,16 @@
 #include "Archive/Archive.hpp"
 
-Archive::Archive(std::ostream& os, const std::string& class_name): _os(&os) {
-    *_os << R"(<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>)" << '\n';
+Archive::Archive(std::ostream& os, const std::string& class_name) : _os(&os) {
+  *_os << R"(<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>)" << '\n';
 
-    setParent(class_name);
-    _class_name = class_name;
+  setParent(class_name);
+  _class_name = class_name;
 }
 
-Archive::Archive(const std::string& file_path, const std::string& class_name): _os(nullptr) {
-    _doc.LoadFile(file_path.c_str());
+Archive::Archive(const std::string& file_path, const std::string& class_name)
+    : _os(nullptr) {
+  _doc.LoadFile(file_path.c_str());
 
-    setParent(class_name);
-    _class_name = class_name;
+  setParent(class_name);
+  _class_name = class_name;
 }
