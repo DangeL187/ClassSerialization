abf40899514fefb807f42bda2b35949cdcf3c6bb
DangeL <92173667+DangeL187@users.noreply.github.com>
2024-06-17 21:27:38 +0300

    Add files via upload

--- include/serialization.hpp
+++ include/serialization.hpp
@@ -4,53 +4,58 @@
 #include <stdexcept>
 #include <utility>
 
+#include "Archive/Archive.hpp"
 #include "Archive/map.h"
 
-#include "Archive/Archive.hpp"
-
 namespace serialization {
-    template <typename T>
-    void serialize(T& object, const std::string& file_path) {
-        std::ofstream ofs(file_path);
-        if (!ofs) throw std::runtime_error("Deserialization failed: could not open file for writing");
+template <typename T>
+void serialize(T& object, const std::string& file_path) {
+  std::ofstream ofs(file_path);
+  if (!ofs)
+    throw std::runtime_error(
+        "Deserialization failed: could not open file for writing");
 
-        Archive ar(ofs, std::string(TypeUtils::Type::name(object)));
-        object.serializeBases(ar, 1);
-        object.serialize(ar, 1);
-    }
-
-    template <typename T>
-    void deserialize(T& object, const std::string& file_path) {
-        auto* ifs = new std::ifstream(file_path);
-        if (!*ifs) throw std::runtime_error("Deserialization failed: could not open file for reading");
-        delete ifs;
-
-        Archive ar(file_path, std::string(TypeUtils::Type::name(object)));
-        object.serializeBases(ar, 1);
-        object.serialize(ar, 1);
-    }
+  Archive ar(ofs, std::string(TypeUtils::Type::name(object)));
+  object.serializeBases(ar, 1);
+  object.serialize(ar, 1);
 }
+
+template <typename T>
+void deserialize(T& object, const std::string& file_path) {
+  auto* ifs = new std::ifstream(file_path);
+  if (!*ifs)
+    throw std::runtime_error(
+        "Deserialization failed: could not open file for reading");
+  delete ifs;
+
+  Archive ar(file_path, std::string(TypeUtils::Type::name(object)));
+  object.serializeBases(ar, 1);
+  object.serialize(ar, 1);
+}
+}  // namespace serialization
 
 #define ADD_TO_ARCHIVE(arg) ar.connect(arg, #arg);
 #define MAP_ADD_TO_ARCHIVE(...) MAP(ADD_TO_ARCHIVE, __VA_ARGS__)
 
-#define DEFINE_SERIALIZE(...) \
-public: \
-    void serialize(Archive& ar, const unsigned int version, bool is_parent = true) { \
-        if (is_parent) ar.setParent(std::string(TypeUtils::Type::name(*this))); \
-        MAP_ADD_TO_ARCHIVE(__VA_ARGS__) \
-        if (is_parent) ar.setParent(); \
-    }
+#define DEFINE_SERIALIZE(...)                                               \
+ public:                                                                    \
+  void serialize(Archive& ar, const unsigned int version,                   \
+                 bool is_parent = true) {                                   \
+    if (is_parent) ar.setParent(std::string(TypeUtils::Type::name(*this))); \
+    MAP_ADD_TO_ARCHIVE(__VA_ARGS__)                                         \
+    if (is_parent) ar.setParent();                                          \
+  }
 
-#define DEFINE_SERIALIZE_BASES(...) \
-public: \
-    void serializeBases(Archive& ar, const unsigned int version) { \
-        __serializeBases<__VA_ARGS__>(ar, version); \
-    } \
-private: \
-    template<typename... Bases> \
-    void __serializeBases(Archive& ar, const unsigned int version) { \
-        (Bases::serialize(ar, version, true), ...); \
-    }
+#define DEFINE_SERIALIZE_BASES(...)                                \
+ public:                                                           \
+  void serializeBases(Archive& ar, const unsigned int version) {   \
+    __serializeBases<__VA_ARGS__>(ar, version);                    \
+  }                                                                \
+                                                                   \
+ private:                                                          \
+  template <typename... Bases>                                     \
+  void __serializeBases(Archive& ar, const unsigned int version) { \
+    (Bases::serialize(ar, version, true), ...);                    \
+  }
 
-#endif //CLASSSERIALIZATION_SERIALIZATION_HPP
+#endif  // CLASSSERIALIZATION_SERIALIZATION_HPP
